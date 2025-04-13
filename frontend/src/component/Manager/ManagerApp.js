7327605f6021d44a575d8b23de55fdf345061edc
DinilDulneth <dinildulneth123@gmail.com>
2025-04-13 19:15:06 +0530

    new update 4-13 II

--- frontend/src/component/Manager/ManagerApp.js
+++ frontend/src/component/Manager/ManagerApp.js
@@ -7,11 +7,37 @@ import ProgressAnalytics from "./component/ProgressAnalytics";
 import ManageTask from "./component/ManageTask";
 import EmployeeProgress from "./component/EmployeeProgress";
 import LeaveApproval from "./component/LeaveApproval";
+import DashboardTemp from "../../component/DashboardTemp";
+
+const links = [
+  { link: "/ManagerDashboard", name: "Dashboard", icon: "bi-house-door" },
+  {
+    link: "/ManagerDashboard/analytics",
+    name: "Progress Analytics",
+    icon: "bi-bar-chart"
+  },
+  {
+    link: "/ManagerDashboard/empAll",
+    name: "Handling Employees",
+    icon: "bi-people"
+  },
+  {
+    link: "/ManagerDashboard/manageTask",
+    name: "Manage Task",
+    icon: "bi-card-checklist"
+  },
+  {
+    link: "/ManagerDashboard/leavesAp",
+    name: "Leave Approval",
+    icon: "bi-check-circle"
+  },
+  { link: "/ManagerDashboard/settings", name: "Settings", icon: "bi-gear" }
+];
 
 function ManagerApp() {
   return (
     <>
-      <MDashboard />
+      <DashboardTemp ArrLinkList={links} />
       <Routes>
         <Route path="/" element={<DashboardOverView />} />
         <Route path="/empAll" element={<FetchEmp />} />
