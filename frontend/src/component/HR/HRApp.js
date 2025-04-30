e8e19faabdc576361eec826144b6b3a27c42007a
Dinil Dulneth <157944899+DinilDulneth@users.noreply.github.com>
2025-04-30 23:31:32 +0530

    Update HRApp.js by dinil

--- frontend/src/component/HR/HRApp.js
+++ frontend/src/component/HR/HRApp.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import FetchEmp from "./component/fetchEmp";
-import ResignationVemp from "./component/ResignationVemp";
 import UserRegister from "./component/UserRegister";
 import DashboardHR from "./component/DashboardHR";
 import SalaryDashboard from "./component/SalaryDashboard";
@@ -12,72 +11,71 @@ import FetchHR from "./component/fetchHR";
 import FetchManager from "./component/fetchManager";
 import HRRegistration from "./component/HRRegistration";
 import ManagerRegistration from "./component/ManagerRegistration";
-import RegistrationDemo from "./component/ResignationDemp";
-import RegistrationUemo from "./component/ResignationUemp";
-import RegistrationVemo from "./component/ResignationVemp";
+import ResignationDemp from "./component/ResignationDemp";
+import ResignationUemp from "./component/ResignationUemp";
+import ResignationVemp from "./component/ResignationVemp";
 import DashboardTemp from "../../component/DashboardTemp";
-import FetchInquiry from "./component/fetchinquiry";
-import FetchAnnouncement from "./component/fetchAnnouncement";
 
 export default function HRApp() {
   const links = [
     // { link: "/", name: "Dashboard", icon: "bi-house-door" },
-    { link: "/HRDashboard/FetchEmp", name: "Employees", icon: "bi-people" },
     {
       link: "/HRDashboard/ResignationVemp",
       name: "Resignation View",
-      icon: "bi-file-earmark",
+      icon: "bi-file-earmark"
     },
+    
+    { link: "/HRDashboard/FetchEmp", name: "Employees", icon: "bi-people" },
+
+    {
+      link: "/HRDashboard/fetchManager",
+      name: "View Manager",
+      icon: "bi-person-badge"
+    
+    },
+
+    {
+      link: "/HRDashboard/FetchHR",
+      name: "HR Records",
+      icon: "bi-clipboard-data"
+    },
+
     {
       link: "/HRDashboard/UserRegister",
       name: "User Registration",
-      icon: "bi-person-plus",
+      icon: "bi-person-plus"
     },
     {
       link: "/HRDashboard/ManagerRegistration",
       name: "Manager Registration",
-      icon: "bi-person-workspace",
-    },
-    {
-      link: "/HRDashboard/fetchManager",
-      name: "View Manager",
-      icon: "bi-person-badge",
+      icon: "bi-person-workspace"
     },
     {
       link: "/HRDashboard/HRRegistration",
       name: "HR Registration",
-      icon: "bi-person-lines-fill",
+      icon: "bi-person-lines-fill"
+
+
     },
-    {
-      link: "/HRDashboard/FetchHR",
-      name: "HR Records",
-      icon: "bi-clipboard-data",
-    },
-    {
-      link: "/HRDashboard/fetchInquiry",
-      name: "Employee Inquiries",
-      icon: "bi-question-circle",
-    },
-    {
-      link: "/HRDashboard/fetchAnnouncement",
-      name: "Announcements",
-      icon: "bi-megaphone",
-    },
+
+
+
+
     {
       link: "/HRDashboard/addSalary",
       name: "Add Salary",
-      icon: "bi-cash-coin",
+      icon: "bi-cash-coin"
     },
     {
       link: "/HRDashboard/fetchSalary",
       name: "View Salary",
-      icon: "bi-wallet2",
+      icon: "bi-wallet2"
     },
     {
       link: "/HRDashboard/SalaryDashboard",
       name: "Salary Dashboard",
-      icon: "bi-bar-chart",
-    },
+      icon: "bi-bar-chart"
+    }
   ];
 
   return (
@@ -88,19 +86,16 @@ export default function HRApp() {
         <Route path="/FetchEmp" element={<FetchEmp />} />
         <Route path="/fetchHR" element={<FetchHR />} />
         <Route path="/fetchManager" element={<FetchManager />} />
-        <Route path="/ResignationVemp" element={<ResignationVemp />} />
         <Route path="/UserRegister" element={<UserRegister />} />
         <Route path="/HRRegistration" element={<HRRegistration />} />
         <Route path="/ManagerRegistration" element={<ManagerRegistration />} />
-        <Route path="/RegistrationDemo" element={<RegistrationDemo />} />
-        <Route path="/RegistrationUemo" element={<RegistrationUemo />} />
-        <Route path="/RegistrationVemo" element={<RegistrationVemo />} />
+        <Route path="/ResignationDemp" element={<ResignationDemp />} />
+        <Route path="/ResignationUemp/:id" element={<ResignationUemp />} />
+        <Route path="/ResignationVemp" element={<ResignationVemp />} />
         <Route path="/SalaryDashboard" element={<SalaryDashboard />} />
         <Route path="/addSalary" element={<AddSalary />} />
         <Route path="/fetchSalary" element={<FetchSalary />} />
         <Route path="/updateSalary/:id" element={<UpdateSalary />} />
-        <Route path="/fetchInquiry" element={<FetchInquiry />} />
-        <Route path="/fetchAnnouncement" element={<FetchAnnouncement />} />
       </Routes>
     </>
   );
